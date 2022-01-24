636f749ce50a21e0f1f7259d31b9307238dd97b8
taranovev <tar_ev@mail.ru>
2022-01-24 20:55:04 +0300

    Edit according to PEP8

--- property/admin.py
+++ property/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
-
 from .models import Flat, Сomplaint, Owner
+
 
 class FlatInline(admin.TabularInline):
     model = Flat.owners.through
     raw_id_fields = ['owner']
 
 
-
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner',]
-    readonly_fields = ['created_at',]
+    search_fields = ['town', 'address', 'owner', ]
+    readonly_fields = ['created_at', ]
     list_display = [
-        'address', 'price', 'new_building', 
+        'address', 'price', 'new_building',
         'construction_year', 'town',
     ]
     list_filter = ['new_building',
@@ -21,8 +20,10 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ['who_likes']
     inlines = [FlatInline]
 
+
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['author', 'flat_complaint']
+
 
 class OwnerAdmin(admin.ModelAdmin):
     list_display = [
